cffeaec973b946ac8c8b39f9d5f88511b3133bb3
so1ve <nn_201312@163.com>
2022-11-25 22:28:32 +0800

    feat: serve shiki assets locally

---
+++ scripts/copy-shiki-assets.ts
@@ -0,0 +1,9 @@
+// Copy shiki-es assets to the public folder
+
+import { join } from "node:path";
+import { copy } from "fs-extra";
+
+const shikiAssetsDir = join(__dirname, "..", "node_modules", "shiki-es", "dist", "assets");
+const publicAssetsDir = join(__dirname, "..", "public", "shiki-assets");
+
+copy(shikiAssetsDir, publicAssetsDir, { recursive: true });
--- src/composables/renderer.ts
+++ src/composables/renderer.ts
@@ -3,7 +3,7 @@ import { getRenderer } from "@dolan-x/markdown-renderer";
 
 export async function useRenderMarkdown (md: string) {
   const { getHighlighter, setCDN } = await import("shiki-es");
-  setCDN("https://unpkg.com/shiki/");
+  setCDN("/shiki-assets/");
   const highlighter = await getHighlighter({ theme: "css-variables" });
   const renderer = getRenderer({ modifyRehype: instance => instance.use(rehypeShiki, { highlighter }) });
   return String(renderer.processSync(md));
