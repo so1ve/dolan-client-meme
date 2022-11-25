a78978df241c79fa35f476d283539099de5a2b25
so1ve <nn_201312@163.com>
2022-11-25 22:21:18 +0800

    chore: update cdn

--- src/composables/renderer.ts
+++ src/composables/renderer.ts
@@ -2,7 +2,8 @@ import rehypeShiki from "@so1ve/rehype-shiki";
 import { getRenderer } from "@dolan-x/markdown-renderer";
 
 export async function useRenderMarkdown (md: string) {
-  const { getHighlighter } = await import("shiki-es");
+  const { getHighlighter, setCDN } = await import("shiki-es");
+  setCDN("https://unpkg.com/shiki/");
   const highlighter = await getHighlighter({ theme: "css-variables" });
   const renderer = getRenderer({ modifyRehype: instance => instance.use(rehypeShiki, { highlighter }) });
   return String(renderer.processSync(md));
