c8cdfdcacaa6ea8f6934a99ceece75af57063bd2
so1ve <nn_201312@163.com>
2022-11-25 22:44:40 +0800

    fix: disable self-hosted assets for now

--- src/composables/renderer.ts
+++ src/composables/renderer.ts
@@ -2,8 +2,7 @@ import rehypeShiki from "@so1ve/rehype-shiki";
 import { getRenderer } from "@dolan-x/markdown-renderer";
 
 export async function useRenderMarkdown (md: string) {
-  const { getHighlighter, setCDN } = await import("shiki-es");
-  setCDN("/shiki-assets/");
+  const { getHighlighter } = await import("shiki-es");
   const highlighter = await getHighlighter({ theme: "css-variables" });
   const renderer = getRenderer({ modifyRehype: instance => instance.use(rehypeShiki, { highlighter }) });
   return String(renderer.processSync(md));
