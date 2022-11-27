48bc214bb71bbbb2ecf1e6caf420e9b9f8b2d8c7
so1ve <nn_201312@163.com>
2022-11-27 12:11:48 +0800

    feat: use api to render markdown

--- src/composables/renderer.ts
+++ src/composables/renderer.ts
@@ -1,9 +1,7 @@
-import rehypeShiki from "@so1ve/rehype-shiki";
-import { getRenderer } from "@dolan-x/markdown-renderer";
-
 export async function useRenderMarkdown (md: string) {
-  const { getHighlighter } = await import("shiki-es");
-  const highlighter = await getHighlighter({ theme: "css-variables" });
-  const renderer = getRenderer({ modifyRehype: instance => instance.use(rehypeShiki, { highlighter }) });
-  return String(renderer.processSync(md));
+  const { data } = await useFetch("/api/render", {
+    method: "POST",
+    body: { md },
+  });
+  return data.value;
 }
