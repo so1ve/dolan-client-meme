48baa8e6dcb553181308db1176f6cc7e9e9d279f
so1ve <nn_201312@163.com>
2022-11-27 12:25:56 +0800

    refactor: import shiki directly

--- src/server/api/render/index.post.ts
+++ src/server/api/render/index.post.ts
@@ -1,9 +1,9 @@
+import { getHighlighter } from "shiki-es";
 import rehypeShiki from "@so1ve/rehype-shiki";
 import { getRenderer } from "@dolan-x/markdown-renderer";
 
 export default defineEventHandler(async (req) => {
   const { md = "" } = await readBody(req);
-  const { getHighlighter } = await import("shiki-es");
   const highlighter = await getHighlighter({ theme: "css-variables" });
   const renderer = getRenderer({ modifyRehype: instance => instance.use(rehypeShiki, { highlighter }) });
   return String(renderer.processSync(md));
