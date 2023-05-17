3773bf2ca2ac3cf6c20abfa24d1cdb1d2818b28a
so1ve <i@mk1.io>
2023-05-17 18:07:45 +0800

    fix: unjs/h3#381

--- src/server/api/render/index.post.ts
+++ src/server/api/render/index.post.ts
@@ -3,7 +3,9 @@ import rehypeShiki from "@so1ve/rehype-shiki";
 import { getRenderer } from "@dolan-x/markdown-renderer";
 
 export default defineEventHandler(async (req) => {
-  const { md = "" } = await readBody(req);
+  // Workaround for https://github.com/unjs/h3/issues/381
+  // @ts-expect-error - req.body is not defined in the type definitions
+  const { md = "" } = await (req.node.req.body || readBody(req));
   const highlighter = await getHighlighter({ theme: "css-variables" });
   const renderer = getRenderer({ modifyRehype: instance => instance.use(rehypeShiki, { highlighter }) });
   return String(await renderer.process(md));
