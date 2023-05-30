ae67e770209f4da8aeff5ee16c74a1bba31c7743
so1ve <i@mk1.io>
2023-05-30 13:47:12 +0800

    chore: remove log

--- src/server/api/render/index.post.ts
+++ src/server/api/render/index.post.ts
@@ -5,7 +5,6 @@ import { getRenderer } from "@dolan-x/markdown-renderer";
 export default defineEventHandler(async (req) => {
   const { md = "" } = await readBody(req);
 
-  console.log(md, typeof md);
   const highlighter = await getHighlighter({ theme: "css-variables" });
   const renderer = getRenderer({
     modifyRehype: (instance) => instance.use(rehypeShiki, { highlighter }),
