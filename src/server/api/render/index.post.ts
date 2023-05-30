c3d8c71d2f783b08e10d4f4e35450f353210d729
so1ve <i@mk1.io>
2023-05-30 13:41:00 +0800

    test

--- src/server/api/render/index.post.ts
+++ src/server/api/render/index.post.ts
@@ -6,6 +6,8 @@ export default defineEventHandler(async (req) => {
   // Workaround for https://github.com/unjs/h3/issues/381
   // @ts-expect-error - req.body is not defined in the type definitions
   const { md = "" } = await (req.node.req.body || readBody(req));
+
+  console.log(md, typeof md);
   const highlighter = await getHighlighter({ theme: "css-variables" });
   const renderer = getRenderer({
     modifyRehype: (instance) => instance.use(rehypeShiki, { highlighter }),
