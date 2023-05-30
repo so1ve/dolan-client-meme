4492bf172119644a2bfbd6978ddec9e279cc44ed
so1ve <i@mk1.io>
2023-05-30 13:44:21 +0800

    ?

--- src/server/api/render/index.post.ts
+++ src/server/api/render/index.post.ts
@@ -3,9 +3,7 @@ import rehypeShiki from "@so1ve/rehype-shiki";
 import { getRenderer } from "@dolan-x/markdown-renderer";
 
 export default defineEventHandler(async (req) => {
-  // Workaround for https://github.com/unjs/h3/issues/381
-  // @ts-expect-error - req.body is not defined in the type definitions
-  const { md = "" } = await (req.node.req.body || readBody(req));
+  const { md = "" } = await readBody(req);
 
   console.log(md, typeof md);
   const highlighter = await getHighlighter({ theme: "css-variables" });
